179d92a20694e853ab99b7351d73d5e46d311ab4
olearybm <90865347+olearybm@users.noreply.github.com>
2024-03-30 14:02:58 -0400

    Created repo and files for SBA 316

---
+++ moviePicks.js
@@ -0,0 +1,52 @@
+let moviePicks = [
+    {
+        title: "Indiana Jones: Raiders of the Lost Ark",
+        genres: ["Action", "Adventure"],
+        img: 'https://m.media-amazon.com/images/M/MV5BNTU2ODkyY2MtMjU1NC00NjE1LWEzYjgtMWQ3MzRhMTE0NDc0XkEyXkFqcGdeQXVyMjM4MzQ4OTQ@._V1_QL75_UY281_CR1,0,190,281_.jpg'
+    },
+    {
+        title: "Black Panther",
+        genres: ["Action", "Adventure"],
+        img: 'https://m.media-amazon.com/images/M/MV5BMTg1MTY2MjYzNV5BMl5BanBnXkFtZTgwMTc4NTMwNDI@._V1_QL75_UX190_CR0,0,190,281_.jpg'
+    },
+    {
+        title: "Star Wars: Episode VII - The Force Awakens",
+        genres: ["Action", "Adventure", "Science Fiction"],
+        img: 'https://m.media-amazon.com/images/M/MV5BOTAzODEzNDAzMl5BMl5BanBnXkFtZTgwMDU1MTgzNzE@._V1_QL75_UY281_CR0,0,190,281_.jpg'
+    },
+    {
+        title: "The Shawshank Redemption",
+        genres: ["Drama"],
+        img: 'https://m.media-amazon.com/images/M/MV5BNDE3ODcxYzMtY2YzZC00NmNlLWJiNDMtZDViZWM2MzIxZDYwXkEyXkFqcGdeQXVyNjAwNDUxODI@._V1_QL75_UX190_CR0,2,190,281_.jpg'
+    },
+    {
+        title: "Titanic",
+        genres: ["Drama", "Romance"],
+        img: 'https://m.media-amazon.com/images/M/MV5BMDdmZGU3NDQtY2E5My00ZTliLWIzOTUtMTY4ZGI1YjdiNjk3XkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_QL75_UX190_CR0,1,190,281_.jpg'
+    },
+    {
+        title: "La La Land",
+        genres: ["Romance", "Musical"],
+        img: 'https://m.media-amazon.com/images/M/MV5BMzUzNDM2NzM2MV5BMl5BanBnXkFtZTgwNTM3NTg4OTE@._V1_QL75_UX190_CR0,0,190,281_.jpg'
+    },
+    {
+        title: "Coming to America",
+        genres: ["Comedy", "Romance"],
+        img: 'https://m.media-amazon.com/images/M/MV5BNGZlNjdlZmMtYTg0MC00MmZkLWIyNDktYmNlOWYzMTkzYWQ1XkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_QL75_UX190_CR0,4,190,281_.jpg'
+    },
+    {
+        title: "Get Out",
+        genres: ["Horror", "Thriller"],
+        img: 'https://m.media-amazon.com/images/M/MV5BMjUxMDQwNjcyNl5BMl5BanBnXkFtZTgwNzcwMzc0MTI@._V1_QL75_UX190_CR0,0,190,281_.jpg'
+    },
+    {
+        title: "Skyfall",
+        genres: ["Action", "Adventure", "Thriller"],
+        img: 'https://m.media-amazon.com/images/M/MV5BMWZiNjE2OWItMTkwNy00ZWQzLWI0NTgtMWE0NjNiYTljN2Q1XkEyXkFqcGdeQXVyNzAwMjYxMzA@._V1_QL75_UX190_CR0,0,190,281_.jpg'
+    },
+    {
+        title: "Jumanji: Welcome to the Jungle",
+        genres: ["Action", "Adventure", "Comedy"],
+        img: 'https://m.media-amazon.com/images/M/MV5BODQ0NDhjYWItYTMxZi00NTk2LWIzNDEtOWZiYWYxZjc2MTgxXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_QL75_UX190_CR0,0,190,281_.jpg'
+    }
+];
